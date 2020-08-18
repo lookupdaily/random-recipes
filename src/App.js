0577e50492927da4ff82094cd96b3761657d1fed
Liz Daly <liz7daly@gmail.com>
2020-08-18 16:35:50 +0100

    Update component to fetch recipes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+  const [recipe, setRecipe] = useState({})
+  const [error, setError] = useState(null)
+
+  async function fetchRecipe() {
+    const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY
+    fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=1`)
+      .then(res => res.json())
+      .then(
+        (res) => { 
+          if (res.status === "failure") {
+            setError(res)
+          } else {
+            setRecipe(res.recipes[0])
+          }
+         }
+      )
+      .catch(error => setError(error))
+  }
+
+  useEffect(()=> {
+    fetchRecipe()
+  },[])
+
+  if(error) {
+    return <div>Error: {error.message}</div>
+  }
   return (
     <div>
       <header>
@@ -9,7 +35,7 @@ const App = () => {
       <button>Show me another recipe</button>
       <article>
         <img src="" title="recipe image" alt="recipe"/>
-        <h3>Recipe Title</h3>
+        <h3>Recipe title</h3>
         <p>Prep time: time here</p>
         <p>summary here</p>
         <p>Source: <a href="#">source here</a></p>
